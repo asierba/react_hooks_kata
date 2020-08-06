409ad822972247f8760223502f3380c117c6e5d6
David Moran <david.moran@thoughtworks.com>
2020-08-06 09:32:23 +0200

    Refactors describe specs to use localStorage for some tests

--- __tests__/components/ContactListSpec.tsx
+++ __tests__/components/ContactListSpec.tsx
@@ -10,63 +10,91 @@ Enzyme.configure({
 });
 
 describe("Contact list functionality", () => {
+
   beforeEach(() => {
     localStorage.clear();
   });
 
-  it("Should render no contacts in initial state", () => {
-    const contactList = mount(<ContactList />);
-    expect(contactList.find(".contact")).toHaveLength(0);
-  });
+  describe("With localStorage", () => {
+    const anyName = "tirateUnPaso";
+    const anyPhone = "3141592";
+    const otherPhone = "31415926";
 
-  it("Should render a specific contact when button is pressed", (done) => {
-    const contactList = mount(<ContactList />);
-    const name = "david";
-    const phone = "123456789";
-
-    contactList
-      .find('[data-id="input-name"]')
-      .simulate("change", { target: { value: name } });
-    contactList
-      .find('[data-id="input-phone"]')
-      .simulate("change", { target: { value: phone } });
-    contactList.find("button").simulate("click");
-
-    setImmediate(() => {
-      expect(contactList.find(".contact")).toHaveLength(1);
-      expect(
-        contactList.find(".contact").first().find('[data-id="name"]').text()
-      ).toBe(name);
-      expect(
-        contactList.find(".contact").first().find('[data-id="phone"]').text()
-      ).toBe(phone);
-      done();
+    beforeEach(() => {
+      localStorage.setItem("contacts", JSON.stringify([
+        new Contact(anyPhone, anyName, false),
+        new Contact(otherPhone, anyName, true)
+      ]));
     });
+
+
+    it("Should load contacts from localstorage", () => {
+      const contactList = mount(<ContactList />);
+
+      expect(contactList.find(".contact")).toHaveLength(2);
+      expect(
+          contactList.find(".contact").first().find('[data-id="name"]').text()
+      ).toBe(anyName);
+      expect(
+          contactList.find(".contact").first().find('[data-id="phone"]').text()
+      ).toBe(anyPhone);
+      expect(
+          contactList.find(".contact").first().hasClass('favorite')
+      ).toBe(false);
+    });
+
+    it("Should add contact as favorite", (done) => {
+      const contactList = mount(<ContactList />);
+
+      contactList.find(".contact").first().find('[data-id="fav"]').simulate('click');
+
+      setImmediate(()=> {
+        expect(
+            JSON.parse(localStorage.getItem('contacts'))[0].isFavorite
+        ).toBe(true);
+        expect(
+            contactList.find(".contact").first().hasClass('favorite')
+        ).toBe(true);
+        done();
+      });
+    });
+
+    it("Should render a fav contact ", () => {
+      const contactList = mount(<ContactList />);
+
+      expect(
+          contactList.find(".contact")
+              .map(node => node.hasClass('favorite'))
+              .some(x => x) // ???
+      ).toBe(true);
+    })
   });
 
-  it("Should save new added contact", (done) => {
-    const contactList = mount(<ContactList />);
-    const name = "david";
-    const phone = "123456789";
+  describe("Without localStorage", () => {
 
-    contactList
-      .find('[data-id="input-name"]')
-      .simulate("change", { target: { value: name } });
-    contactList
-      .find('[data-id="input-phone"]')
-      .simulate("change", { target: { value: phone } });
-    contactList.find("button").simulate("click");
+    it("Should save new added contact", (done) => {
+      const contactList = mount(<ContactList />);
+      const name = "david";
+      const phone = "123456789";
 
-    setImmediate(() => {
-      expect(localStorage.getItem("contacts")).toBe(
-        JSON.stringify([{ phone, name, isFavorite: false }])
-      );
+      contactList
+          .find('[data-id="input-name"]')
+          .simulate("change", { target: { value: name } });
+      contactList
+          .find('[data-id="input-phone"]')
+          .simulate("change", { target: { value: phone } });
+      contactList.find("button").simulate("click");
 
-      expect(JSON.parse(localStorage.getItem("contacts"))).toEqual(
-        expect.arrayContaining([expect.objectContaining({ name })])
-      );
-      expect(JSON.parse(localStorage.getItem("contacts")))
-        .toMatchInlineSnapshot(`
+      setImmediate(() => {
+        expect(localStorage.getItem("contacts")).toBe(
+            JSON.stringify([{ phone, name, isFavorite: false }])
+        );
+
+        expect(JSON.parse(localStorage.getItem("contacts"))).toEqual(
+            expect.arrayContaining([expect.objectContaining({ name })])
+        );
+        expect(JSON.parse(localStorage.getItem("contacts")))
+            .toMatchInlineSnapshot(`
         Array [
           Object {
             "isFavorite": false,
@@ -76,61 +104,41 @@ describe("Contact list functionality", () => {
         ]
       `);
 
-      done();
+        done();
+      });
+    });
+
+    it("Should render no contacts in initial state", () => {
+      const contactList = mount(<ContactList />);
+      expect(contactList.find(".contact")).toHaveLength(0);
+    });
+
+    it("Should render a specific contact when button is pressed", (done) => {
+      const contactList = mount(<ContactList />);
+      const name = "david";
+      const phone = "123456789";
+
+      contactList
+          .find('[data-id="input-name"]')
+          .simulate("change", { target: { value: name } });
+      contactList
+          .find('[data-id="input-phone"]')
+          .simulate("change", { target: { value: phone } });
+      contactList.find("button").simulate("click");
+
+      setImmediate(() => {
+        expect(contactList.find(".contact")).toHaveLength(1);
+        expect(
+            contactList.find(".contact").first().find('[data-id="name"]').text()
+        ).toBe(name);
+        expect(
+            contactList.find(".contact").first().find('[data-id="phone"]').text()
+        ).toBe(phone);
+        done();
+      });
     });
   });
 
-  it("Should load contacts from localstorage", () => {
-    const name = "tirateUnPaso";
-    const phone = "3141592";
 
-    localStorage.setItem("contacts", JSON.stringify([{ phone, name }]));
 
-    const contactList = mount(<ContactList />);
-
-    expect(contactList.find(".contact")).toHaveLength(1);
-    expect(
-      contactList.find(".contact").first().find('[data-id="name"]').text()
-    ).toBe(name);
-    expect(
-      contactList.find(".contact").first().find('[data-id="phone"]').text()
-    ).toBe(phone);
-    expect(
-        contactList.find(".contact").first().hasClass('favorite')
-    ).toBe(false);
-  });
-
-  it("Should add contact as favorite", (done) => {
-    const name = "tirateUnPaso";
-    const phone = "3141592";
-
-    localStorage.setItem("contacts", JSON.stringify([new Contact(phone, name)]));
-
-    const contactList = mount(<ContactList />);
-
-    contactList.find(".contact").first().find('[data-id="fav"]').simulate('click');
-
-    setImmediate(()=> {
-      expect(
-          JSON.parse(localStorage.getItem('contacts'))[0].isFavorite
-      ).toBe(true);
-      expect(
-          contactList.find(".contact").first().hasClass('favorite')
-      ).toBe(true);
-      done();
-    });
-  });
-
-  it("Should render a fav contact ", () => {
-    const name = "tirateUnPaso";
-    const phone = "3141592";
-    const isFavorite = true;
-
-    localStorage.setItem("contacts", JSON.stringify([new Contact(phone, name, isFavorite)]));
-    const contactList = mount(<ContactList />);
-
-    expect(
-        contactList.find(".contact").first().hasClass('favorite')
-    ).toBe(true);
-  })
 });
