34f805f21ad3234f0f8f3e18ae84df8a0de9d5f6
pedropenalver <pedropenalver@packlink.com>
2020-09-10 10:00:15 +0200

    feat: add usememo to check isdisable

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import {useCallback, useEffect, useState} from 'react';
-import {Contact} from '../../models/Contact';
+import { useCallback, useEffect, useState, useMemo } from 'react';
+import { Contact } from '../../models/Contact';
 import Table from 'react-bootstrap/Table';
 
 function addToList(contacts: Contact[], inputPhone: string, inputName: string) {
@@ -9,54 +9,61 @@ function addToList(contacts: Contact[], inputPhone: string, inputName: string) {
     return allContacts;
 }
 
-
 export const ContactList = () => {
     const [contacts, setContacts] = useState<Contact[]>(JSON.parse(localStorage.getItem('contacts') || '[]'));
     const [inputName, setInputName] = useState('');
     const [inputPhone, setInputPhone] = useState('');
 
-    const setAsFav = useCallback((favContact: Contact) =>  {
-        favContact.isFavorite = !favContact.isFavorite;
-        setContacts(contacts.map(contact => contact.phone === favContact.phone ? favContact : contact));
-    },[contacts]);
+    const setAsFav = useCallback(
+        (favContact: Contact) => {
+            favContact.isFavorite = !favContact.isFavorite;
+            setContacts(contacts.map((contact) => (contact.phone === favContact.phone ? favContact : contact)));
+        },
+        [contacts]
+    );
 
     useEffect(() => {
         localStorage.setItem('contacts', JSON.stringify(contacts));
-    },[contacts]);
+    }, [contacts]);
 
-
-    const isContactAlready = () => !!contacts.find((contact) => contact.phone === inputPhone);
-    const isDisabled = isContactAlready();
+    const isDisabled = useMemo((): boolean => !!contacts.find((contact) => contact.phone === inputPhone), [
+        contacts,
+        inputPhone,
+    ]);
 
     return (
         <>
             <Table bordered hover>
                 <thead>
-                <tr>
-                    <td>Nombre</td>
-                    <td>Número</td>
-                    <td>Fav!</td>
-                </tr>
+                    <tr>
+                        <td>Nombre</td>
+                        <td>Número</td>
+                        <td>Fav!</td>
+                    </tr>
                 </thead>
                 <tbody>
-                {contacts.map(contact => {
-                    return (
-                        <tr className={ `contact ${contact.isFavorite ? "favorite" : ""}` } key={contact.phone}>
-                            <td role="name">{contact.name}</td>
-                            <td role="phone">{contact.phone}</td>
-                            <td><button data-id="fav" onClick={() => setAsFav(contact)}>Fav</button></td>
-                        </tr>
-                    )
-                })}
+                    {contacts.map((contact) => {
+                        return (
+                            <tr className={`contact ${contact.isFavorite ? 'favorite' : ''}`} key={contact.phone}>
+                                <td role="name">{contact.name}</td>
+                                <td role="phone">{contact.phone}</td>
+                                <td>
+                                    <button data-id="fav" onClick={() => setAsFav(contact)}>
+                                        Fav
+                                    </button>
+                                </td>
+                            </tr>
+                        );
+                    })}
                 </tbody>
             </Table>
             <label htmlFor="input-name">Nombre</label>
-            <input type="text" id="input-name" onChange={(event: any) => setInputName(event.target.value) } />
+            <input type="text" id="input-name" onChange={(event: any) => setInputName(event.target.value)} />
             <label htmlFor="input-phone">Número</label>
-            <input type="text" id="input-phone" onChange={(event: any) => setInputPhone(event.target.value) } />
-            <button
-                disabled={isDisabled}
-                onClick={() => setContacts(addToList(contacts, inputPhone, inputName))}>Añade nuevo contacto</button>
+            <input type="text" id="input-phone" onChange={(event: any) => setInputPhone(event.target.value)} />
+            <button disabled={isDisabled} onClick={() => setContacts(addToList(contacts, inputPhone, inputName))}>
+                Añade nuevo contacto
+            </button>
         </>
     );
-}
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import App from "./components/App";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './components/app';
 import 'bootstrap/dist/css/bootstrap.css';
-import {Contact} from "../models/Contact";
+import { Contact } from '../models/Contact';
 
-export interface IState{
-    contacts: Contact[]
+export interface IState {
+    contacts: Contact[];
 }
 
 const Index = () => {
     return <App />;
 };
 
-ReactDOM.render(<Index />, document.getElementById("root"));
+ReactDOM.render(<Index />, document.getElementById('root'));
